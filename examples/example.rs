7626298256624861239dfe9c17f6e1306b6b3a59
baekhyun <543683487@qq.com>
2021-03-07 00:46:28 +0800

    z

---
+++ examples/example.rs
@@ -0,0 +1,7 @@
+use fmyers::myers;
+
+fn main(){
+    let a = "ab";
+    let b = "b";
+    myers(a.as_bytes(),b.as_bytes());
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,16 @@
-fn myers<T>(src:&[T],dst:&[T])
+pub fn myers<T>(src:&[T],dst:&[T])
     where T:PartialEq
 {
-       
-}
-
-pub struct VecWrapper{
-    v_:Vec<Vec<usize>>
-}
-
-impl VecWrapper{
-    pub fn new(m:usize,n:usize)->Self{
-        let total = m+n;
-        let mut v:Vec<Vec<usize>> = Vec::new();
-        for i in 0..total{
-            let mut inner:Vec<usize> = Vec::new();
-            (0..total).map(|_|{inner.push(0)}).collect::<()>();
-            v.push(inner);
-        }
-        Self{
-            v_:v
+    let m  = src.len();
+    let n = dst.len();
+    let total = m+n;
+    let mut v:Vec<Vec<usize>> = Vec::new();
+    for d in 0..total{
+        v.push(Vec::new());
+        let mut k = - (d as isize);
+        while k<=d as isize{
+            println!("{}",k);
+            k+=2;
         }
     }
 }
